9f337316919a04999cd155f0b542ee9a7f27ccf8
orik1504 <orik1504@gmail.com>
2021-01-14 19:50:07 +0200

    ✨added player class

---
+++ player.py
@@ -0,0 +1,36 @@
+import random
+class Player():
+    
+    def __init__(self, name, money):
+        welcome = ("Welcome {}")
+        print (welcome.format(name))
+        self.player_cards = []
+        self.__start()
+        self.__sum = 0
+        self.__sum_of_cards()
+        self.money = money
+        self.offer = 0
+        self.sum_bets = 0
+
+    
+    def drew_card(self,):
+        self.card = random.randint(1,13)
+        self.player_cards.append(self.card)
+    
+    def __start (self,):
+        for i in range(2):
+            self.drew_card()
+    
+    def __sum_of_cards(self,):
+        for item in self.player_cards:
+            self.__sum+=item
+
+    def bet (self, offer):
+        self.money -= offer
+
+    def sum_of_bets(self,):
+        self.sum_bets += self.offer
+
+    
+
+    
