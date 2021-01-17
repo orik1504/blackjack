ff6e1ebaed29a85b410da91ab3d51c5f2fd52812
orik1504 <orik1504@gmail.com>
2021-01-17 09:59:18 +0200

    ✨🐛added print all and bug fixes

--- player.py
+++ player.py
@@ -4,8 +4,7 @@ import random
 class Player():
 
     def __init__(self, name,):
-        welcome = ("Welcome {}")
-        print(welcome.format(name))
+        print(f"Welcome {name}")
         self.cards = []
         self.sum = 0
         self.money = 1000
@@ -15,17 +14,17 @@ class Player():
 
     def drew_card(self,):
         self.__card = random.randint(1, 13)
-        self.turn_13_to_10()
-        self.one_or_eleven()
-        self.sum_of_cards()
+        self.__turn_13_to_10()
+        self.__one_or_eleven()
+        self.__sum_of_cards()
 
-    def one_or_eleven(self,):
+    def __one_or_eleven(self,):
         if self.__card == 1:
             print("Do you want that card will be 1 or 11?")
             self.__card = int(input())
         self.cards.append(self.__card)
 
-    def turn_13_to_10(self):
+    def __turn_13_to_10(self):
         if self.__card > 10:
             self.__card = 10
 
@@ -33,7 +32,7 @@ class Player():
         for i in range(2):
             self.drew_card()
 
-    def sum_of_cards(self,):
+    def __sum_of_cards(self,):
         self.sum += self.__card
 
     def bet(self, offer):
@@ -43,10 +42,16 @@ class Player():
         self.sum_bets += self.offer
 
     def choice(self,):
-        print("What is your play: \n 1.drew card \n 2.bet")
+        print("What is your play: \n 1.drew card \n 2.bet \n 3.Nothing")
         self.user_input = input()
         if self.user_input == "1":
             self.drew_card()
-        else:
+        elif self.user_input == "2":
             offer = input("What is your offer?")
             self.bet(offer)
+        else:
+            pass
+
+    def print_all(self,):
+        print(f"Your cards are:{self.cards}",
+              f"Your sum of bets is:{self.sum_bets}", f"You have {self.money} dollars")
