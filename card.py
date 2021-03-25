99780bcc011ecca70fb9a99a955d1c97c4bfe43c
orik1504 <orik1504@gmail.com>
2021-03-25 22:37:08 +0200

    ✨ added set card func

--- card.py
+++ card.py
@@ -13,3 +13,9 @@ class Card():
         self.__check_valid_suit(random.choice(self.VALID_SUITS))
         return self.number, self.suit
 
+    def set_card(self, number: int, suit: str):
+        """ Sets a card object """
+        self.__check_valid_number(number)
+        self.__check_valid_suit(suit)
+        return self.number, self.suit
+
