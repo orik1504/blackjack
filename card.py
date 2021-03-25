4797ea4258459f179296d4aea2bc76aa78e2f634
orik1504 <orik1504@gmail.com>
2021-03-25 22:36:52 +0200

    ✨ added get random card func

--- card.py
+++ card.py
@@ -1,6 +1,15 @@
+import random
+
+
 class Card():
     VALID_SUITS = ("hearts",
                    "clubs",
                    "diamonds",
                    "spades", )
+
+    def get_random_card(self,):
+        """ Creates a random card object"""
+        self.__check_valid_number(random.randint(1, 13))
+        self.__check_valid_suit(random.choice(self.VALID_SUITS))
+        return self.number, self.suit
 
