2b3a8b45d6a7e8a01d5bc151bcc7f8082e8bf48e
orik1504 <orik1504@gmail.com>
2021-03-25 22:35:02 +0200

    ✨ added convert to ten func

--- main_game.py
+++ main_game.py
@@ -2,10 +2,12 @@
 from player import Player
 from dealer import Dealer
 
-# -- VARIABLES -- #
-run = True
-temp = input("Enter your name")
-name = Player(temp)
+    def _convert_to_ten(self, card):
+        ''' Player can't have a card that is more than 13 so every time he gets above 10 the card must convert itself to 10, That is just for getting in statistics'''
+        if card > 10:
+            return 10
+        else:
+            return card
 
 name.print_all()
 
