47110fe83a6ac2c796f038d493a3fb628c39262c
orik1504 <orik1504@gmail.com>
2021-03-27 15:19:42 +0300

    ✨ New card class

--- card.py
+++ card.py
@@ -2,32 +2,66 @@ import random
 
 
 class Card():
-    VALID_SUITS = ("hearts",
-                   "clubs",
-                   "diamonds",
-                   "spades", )
 
-    def get_random_card(self,):
-        """ Creates a random card object"""
-        self.number = random.randint(1, 13)
-        self.suit = random.choice(self.VALID_SUITS)
-        return self.number, self.suit
+    VALID_SUITS = (
+        "hearts",
+        "clubs",
+        "diamonds",
+        "spades",
+    )
 
-    def set_card(self, number: int, suit: str):
-        """ Sets a card object """
-        self.__check_valid_number(number)
-        self.__check_valid_suit(suit)
-        return self.number, self.suit
+    __number, __suit = None, None
 
-    def __check_valid_number(self, number):
-        """ Checks if number of card is valid """
-        if not number <= 13 and number > 0:
+    def __init__(self, number, suit):
+        self.number = number
+        self.suit = suit
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, number):
+
+        if not isinstance(number, int):
+            raise TypeError(
+                f"Card number must be an integer (not {type(number)})")
+
+        if number > 13 and number > 0:
             raise ValueError(
                 f"value can't be {number}, must be between 1 to 13")
-        self.number = number
 
-    def __check_valid_suit(self, suit):
-        """ Checks if suit of card is valid """
-        if not suit in self.VALID_SUITS:
+        self.__number = number
+
+    @property
+    def suit(self):
+        return self.__suit
+
+    @suit.setter
+    def suit(self, suit):
+        if suit not in self.VALID_SUITS:
             raise ValueError("Suit is not valid")
-        self.suit = suit
+
+        self.__suit = suit
+
+    def __str__(self,):
+        return f"Card({self.number} - {self.suit})"
+
+
+class CardManager:
+
+    @staticmethod
+    def random_card() -> Card:
+        number = random.randint(1, 13)
+        suit = random.choice(Card.VALID_SUITS)
+        return Card(number, suit)
+
+
+def main():
+    x = CardManager.random_card()
+    x.suit = "bolbol"
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
