bc6578154b58a160ff0c0ee7843a4d722ddb1b39
orik1504 <orik1504@gmail.com>
2021-03-27 17:38:16 +0300

    ✨ added blackjackcard class

--- card.py
+++ card.py
@@ -57,6 +57,34 @@ class CardManager:
         return Card(number, suit)
 
 
+class BlackJackCard():
+
+    def __init__(self, card: Card):
+        self.card = card
+        self.card_value = self.__generate_card_value(card)
+
+    @staticmethod
+    def __generate_card_value(card: Card) -> int:
+        """ Recives a card instance and returns its value. """
+
+        if card.number > 10:
+            return 10
+
+        if card.number == 1:
+            return BlackJackCard.__ace_card_value()
+
+        return card.number
+
+    @staticmethod
+    def __ace_card_value():
+        VALID_ANSWERS = (1, 11)
+        player_input = input("What do you want the card to be, 1 or 11")
+        if player_input not in VALID_ANSWERS:
+            raise ValueError(
+                f" Value can't be {player_input}, must be 1 or 11")
+        return player_input
+
+
 def main():
     x = CardManager.random_card()
     x.suit = "bolbol"
