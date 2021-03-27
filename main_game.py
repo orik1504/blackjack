5b049378d2a3aa2ca967d2e92ea22d998eb4a9af
orik1504 <orik1504@gmail.com>
2021-03-27 23:34:11 +0300

    ✨ New card functions

--- main_game.py
+++ main_game.py
@@ -5,13 +5,6 @@ class Game():
     def __init__(self):
         pass
 
-    def _convert_to_ten(self, card):
-        ''' Player can't have a card that is more than 13 so every time he gets above 10 the card must convert itself to 10, That is just for getting in statistics'''
-        if card > 10:
-            return 10
-        else:
-            return card
-
     def _one_or_eleven(self, card):
         ''' Player has the option to choose if an Ace that he got will be a "1" or "11" '''
         if card == 1:
@@ -23,6 +16,11 @@ class Game():
                 raise ValueError("The value must be 1 or 11!")
         else:
             return card
+
+    def _split(self, player_start_cards):
+        """ if the player got 2 of the same card (card number) he can split the piles
+        to 2 separated piles """
+        pass
 
     def __check_if_21(self, player) -> bool:
         """ if the player got 21 he won automaticly """
